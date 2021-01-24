afdcc7e33dfda88245bcde1faeac6865ee6914e5
kevu12 <73261110+kevu12@users.noreply.github.com>
2021-01-23 20:30:19 -0500

    Add files via upload

---
+++ Template-Kill-the-Monster-main (1)/Template-Kill-the-Monster-main/sketch.js
@@ -0,0 +1,25 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  MouseConstraints = Matter.MouseConstraints,
+  Bodies = Matter.Bodies;
+
+function preload() {
+//preload the images here
+backgroundImg = loadImage("sprites/GamingBackground.png");
+mono1 = loadImage("sprites/Monster-01.png");
+mono2 = loadImage("sprites/Monster-02.png");
+superh1 = loadImage("sprites/Superhero-01.png");
+superh2 = loadImage("sprites/Superhero-02.png");
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+}
+
